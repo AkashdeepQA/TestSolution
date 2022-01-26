f146b2f157d90b47ff39db0d4e04433a3ef527ed
adoTrainer <akashdeep.ado@outlook.com>
2022-01-26 19:32:31 +0000

    Add project files.

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestSolution;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public DevCode obj = new DevCode();
+
+        [TestMethod]
+        public void VerifyEmpName()
+        {
+            obj.NewEmployee("Dave", "001");
+            Assert.AreEqual("Dave", obj.empName);
+        }
+
+        [TestMethod]
+        public void VerifyEmpId()
+        {
+            obj.NewEmployee("John", "002");
+            Assert.AreEqual("002", obj.empId);
+        }
+    }
+}
---
+++ TestSolution/DevCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSolution
+{
+    public class DevCode
+    {
+        public string empName;
+        public string empId;
+        public void NewEmployee(string name, string id)
+        {
+            empName = name;
+            empId = id;
+        }
+    }
+}
